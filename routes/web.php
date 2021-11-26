fc3f01802ac5c45e532e34176d339b262f1268a0
AldiZulfikar <aldizulfikar04@gmail.com>
2021-11-26 08:28:28 +0700

    login admin

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+
+Auth::routes(['register'=>false]);
+
+
+
+
